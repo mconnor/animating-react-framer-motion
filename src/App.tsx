8906bfb25dc35b3f75e531a4c0e6a1aef8abe9e8
mike connor <mike@rocketnumber9.org>
2020-03-15 17:30:25 -0500

    toggle opacity, animation slider

--- src/App.tsx
+++ src/App.tsx
@@ -10,49 +10,52 @@ import black from './black.png';
 import green from './green.png';
 
 
-type Props = { title:string};
+type Props = { title: string };
 type State = {
     value: number,
-  };
+};
 
-function App({ title }:Props) {
-    const [ value, setValue ] = useState<string>()
-	return (
-		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{duration: 1}}
-		>
-			<Header>
-				<Menu />
-				<h1>Header</h1>
-			</Header>
-			<Container>
-				<motion.h2>Super Cool</motion.h2>
-                <input type="range" min="-100" max="100" 
-                    value={value} 
+function App({ title }: Props) {
+    const [value, setValue] = useState<string>('0')
+    const [isToggled, setToggle] = useState(1)
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+        >
+            <Header>
+                <Menu />
+                <h1>Header</h1>
+            </Header>
+            <Container>
+                <motion.h2
+                    animate={{ opacity: isToggled, x: parseInt(value) }}>Super Cool</motion.h2>
+                <input type="range" min="-100" max="100"
+                    value={value}
                     onChange={(e) => setValue(e.target.value)} />
-				<CardGrid>
-					<Card style={{ background: 'var(--purp)' }}>
-						<h3>Some card</h3>
-						<img src={purp} />
-					</Card>
-					<Card style={{ background: 'var(--blue)' }}>
-						<h3>Some card</h3>
-						<img src={blue} />
-					</Card>
-					<Card style={{ background: 'var(--black)' }}>
-						<h3>Some card</h3>
-						<img src={black} />
-					</Card>
-					<Card style={{ background: 'var(--green)' }}>
-						<h3>Some card</h3>
-						<img src={green} />
-					</Card>
-				</CardGrid>
-			</Container>
-		</motion.div>
-	);
+                <button onClick={() => setToggle(prevValue => prevValue ? 0 : 1)}>toggle</button>
+                <CardGrid>
+                    <Card style={{ background: 'var(--purp)' }}>
+                        <h3>Some card</h3>
+                        <img src={purp} />
+                    </Card>
+                    <Card style={{ background: 'var(--blue)' }}>
+                        <h3>Some card</h3>
+                        <img src={blue} />
+                    </Card>
+                    <Card style={{ background: 'var(--black)' }}>
+                        <h3>Some card</h3>
+                        <img src={black} />
+                    </Card>
+                    <Card style={{ background: 'var(--green)' }}>
+                        <h3>Some card</h3>
+                        <img src={green} />
+                    </Card>
+                </CardGrid>
+            </Container>
+        </motion.div>
+    );
 }
 
 export default App;
